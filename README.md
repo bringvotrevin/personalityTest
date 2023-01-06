a70a8b513b5f04325222b982b438e234e4cb4c1e
dim <dim@student.42seoul.kr>
2023-01-07 05:04:49 +0900

    Feat: 기본 파일 생성

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# personalityTest
+# personalityTest 성격유형검사 테스트 만들기
---
+++ js/data.js
@@ -0,0 +1,39 @@
+const questionList = Object.freeze([
+	{
+		question: '1번질문',
+		answers: [
+			{ answer: '1-1', type: ['a', 'b', 'c']},
+			{ answer: '1-2', type: ['a', 'b', 'c']},
+		]
+	},
+	{
+		question: '2번질문',
+		answers: [
+			{ answer: '2-1', type: ['a', 'b', 'c']},
+			{ answer: '2-2', type: ['a', 'b', 'c']},
+		]
+	},
+	{
+		question: '3번질문',
+		answers: [
+			{ answer: '3-1', type: ['a', 'b', 'c']},
+			{ answer: '3-2', type: ['a', 'b', 'c']},
+		]
+	},
+
+])
+
+const resultList = Object.freeze([
+	{
+		type: 'one',
+		description: '1번 결과',
+	},
+	{
+		type: 'two',
+		description: '2번 결과',
+	},
+	{
+		type: 'three',
+		description: '3번 결과',
+	},
+]);
