bb8519fa5123ade8bcbfaf4392adc13955f30624
dim <dim@student.42seoul.kr>
2023-01-09 04:59:58 +0900

    Feat: 시작페이지 버튼 구비

---
+++ js/main.js
@@ -0,0 +1,8 @@
+const main = document.querySelector('#main');
+const question = document.querySelector('#question');
+const result = document.querySelector('#result');
+
+const start = () => {
+	main.style.display = 'none';
+	question.style.display = 'block';
+}
