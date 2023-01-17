606d3894fd74e74c5de1aaa0176cb89848e447b5
dim <dim@student.42seoul.kr>
2023-01-17 21:22:06 +0900

    Design: 배경색, 테두리 수정

--- js/result.js
+++ js/result.js
@@ -14,10 +14,16 @@ const displayResult = (userAnswer) => {
 	resultPart.insertBefore(resultDiv, shareButton);
 	// resultPart.appendChild(resultDiv);
 	resultDiv.innerHTML = userAnswer.description;
+	resultDiv.style = "width: 50%";
 	resultDiv.style.height = "40%";
-	resultDiv.style.margin = "30px 30px";
+	resultDiv.style.margin = "30px auto";
 	resultDiv.style.padding = "10px";
+	resultDiv.style.border = "px solid #008C7A";
+	resultDiv.style.borderRadius = "10px";
+	resultDiv.style.backgroundColor = '#5EB8AB';
+	// resultDiv.style = "background-image: linear-gradient(60deg, #64b3f4 0%, #c2e59c 100%)";
 }
+
 
 const addShareButton = () => {
 
