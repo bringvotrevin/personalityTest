d0f8ae31ff3a47a0ce95bc94572d3e73a9e961d3
dim <dim@student.42seoul.kr>
2023-01-17 18:58:33 +0900

    Feat: 공유하기 페이지 이미지 수정

--- js/share.js
+++ js/share.js
@@ -8,7 +8,7 @@ const kakaoShare = () => {
       title: 'QnA Test',
       description: 'by dim',
       imageUrl:
-        '../img/bb.jpg',
+        './img/bb.jpg',
       link: {
         // [내 애플리케이션] > [플랫폼] 에서 등록한 사이트 도메인과 일치해야 함
         mobileWebUrl: url,
