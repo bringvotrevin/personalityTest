106b5cbb6b41487cb13e15d5b62034b0ba241e14
dim <dim@student.42seoul.kr>
2023-01-18 16:52:57 +0900

    Style: 배경, 결과본문 색상 변경

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
 
-
 const start = () => {
 	qna();
 }
--- js/result.js
+++ js/result.js
@@ -12,16 +12,8 @@ const displayResult = (userAnswer) => {
 	const shareButton = document.getElementById('kakaotalk-sharing-btn');
 
 	resultPart.insertBefore(resultDiv, shareButton);
-	// resultPart.appendChild(resultDiv);
+	resultDiv.className = "resultDiv";
 	resultDiv.innerHTML = userAnswer.description;
-	resultDiv.style.width =  "50%";
-	resultDiv.style.height = "300px";
-	resultDiv.style.margin = "30px auto";
-	resultDiv.style.padding = "10px";
-	resultDiv.style.border = "px solid #008C7A";
-	resultDiv.style.borderRadius = "10px";
-	resultDiv.style.backgroundColor = '#5EB8AB';
-	// resultDiv.style = "background-image: linear-gradient(60deg, #64b3f4 0%, #c2e59c 100%)";
 }
 
 
